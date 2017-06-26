a2b24ceaf7de5b5437c25b9a4e3bfc787ec17c60
Avery Biskup <averybiskup@gmail.com>
2017-06-27 07:57:56 +0800

    Building Progress

--- portfolioNew.js
+++ portfolioNew.js
@@ -2,6 +2,8 @@
 
 $(document).ready(function() {
 
+
+// pull down menu
   let pullDown = (div) => { $(div).toggleClass("down") }
 
   var test = 0;
@@ -22,26 +24,29 @@ $(document).ready(function() {
       setTimeout(pullDown, 200, '#dropNav')
     }
   });
+
   let to;
   let rad = 0;
-  let divClass
+  let divClass;
   $('.navItem').hover(function() {
-    rad = 0;
     divClass = '.' + $(this).attr('class').split(' ')[1]
-    borderRad(divClass)
-    to = setInterval(borderRad, 500, divClass)
-    //console.log('.' + ($(this).attr('class').split(' ')[1]))
+    addBorder(divClass, 0, 'border-bottom')
+    setTimeout(addBorder, 50, divClass, 0, 'border-right')
+    setTimeout(addBorder, 100, divClass, 0, 'border-top')
+    setTimeout(addBorder, 150, divClass, 0, 'border-left')
   }, function() {
-    clearInterval(to)
-  });
+    setTimeout(addBorder, 1, divClass, 1, 'border-right')
+    setTimeout(addBorder, 50, divClass, 1, 'border-top')
+    setTimeout(addBorder, 100, divClass, 1, 'border-left')
+    setTimeout(addBorder, 150, divClass, 1, 'border-bottom')
+  })
 
-  function borderRad(div) {
-    rad++
-    if (rad <= 50) {
-      setTimeout(borderRad, 100, div)
-      $(div).parents('li').hover().css('border-radius', rad)
-    }
+  let addBorder = function(div, test, dir) { (test == 0) ? $(div).hover().css(dir, '2px solid black') : $(div).hover().css(dir, '2px solid transparent') }
 
-  }
+
+
+
+
+
 
 });
