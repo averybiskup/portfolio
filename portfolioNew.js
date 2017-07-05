66a07da3b695d48fa51e43623f477a8fad827fdb
Avery Biskup <averybiskup@gmail.com>
2017-07-05 11:50:52 +0800

    added to projects part

--- portfolioNew.js
+++ portfolioNew.js
@@ -1,59 +1,52 @@
 // CURRENT PORTFOLIO
 
-$(document).ready(function() {
-
-
+$(document).ready(() => {
 // pull down menu
-  let pullDown = (div) => { $(div).toggleClass("down") }
+  let pullDown = (div) => { $(div).toggleClass('down') }
 
-  var test = 0;
-  $(window).resize(function() {
+  var test = 0
+  $(window).resize(() => {
     if ($(window).width() > 900) {
-      $("#dropNav").removeClass('down').css('display', 'block')
+      $('#dropNav').removeClass('down').css('display', 'block')
       $('#navLi').css('display', 'inline')
       $('.navItem').css('display', 'inline')
-      test = 1;
-    } else {
-      ($('#dropNav').hasClass('down') == false) ? [$('#dropNav').css('display', 'none'), test = 0] : ''
-    }
+      test = 1
+    } else { ($('#dropNav').hasClass('down') === false) ? [$('#dropNav').css('display', 'none'), test = 0] : '' }
   })
-  $('.nav-toggle').on('click', function() {
-    if (test == 0) {
+  $('.nav-toggle').on('click', () => {
+    if (test === 0) {
       $('.navItem').css('display', 'block')
       $('#dropNav').css('display', 'block')
       setTimeout(pullDown, 200, '#dropNav')
     }
-  });
+  })
 
-
-
-  let toggleBorder = function(value) {
+  let toggleBorder = (value) => {
     setTimeout(addBorder, 1, divClass, value, 'border-right')
     setTimeout(addBorder, 100, divClass, value, 'border-top')
     setTimeout(addBorder, 200, divClass, value, 'border-left')
     setTimeout(addBorder, 300, divClass, value, 'border-bottom')
-  };
+  }
 
-  let to;
-  let rad = 0;
-  let divClass;
-  $('.navItem').hover(function() {
-    if ($('#dropNav').hasClass('down') == false) {
+  let divClass
+  $('.navItem').hover( function() {
+    if ($('#dropNav').hasClass('down') === false) {
       divClass = '.' + $(this).attr('class').split(' ')[1]
       toggleBorder(0)
     }
-  }, function() {
-    if ($('#dropNav').hasClass('down') == false) {
-      toggleBorder(1)
-    }
-  })
+  }, () => { if ($('#dropNav').hasClass('down') === false) toggleBorder(1) })
+  let addBorder = (div, test, dir) => { (test === 0) ? $(div).hover().css(dir, '2px solid black') : $(div).hover().css(dir, '2px solid transparent') }
 
-  let addBorder = function(div, test, dir) { (test == 0) ? $(div).hover().css(dir, '2px solid black') : $(div).hover().css(dir, '2px solid transparent') }
+  let improving = ['javascript', 'css3', 'html5', 'jquery', 'python', 'atom', 'git', 'github']
+  let learning = ['webpack', 'bootstrap', 'sass', 'nodejs', 'babel', 'react']
+  let placeIcons = () => {
+    for (var i in improving) { $('.improving').append('<div class="flexIcon"><i class="devicon-' + improving[i] + '-plain dev ' + improving[i] + '"></i></div>') }
+    for (var j in learning) { $('.learning').append('<div class="flexIcon"><i class="devicon-' + learning[j] + '-plain dev ' + learning[j] + '"></i></div>') }
+  }
+  placeIcons()
+
+  $(window).on('scroll', function() { if ($(window).scrollTop() > 680 && $(window).scrollTop() < 900) $('.flexImages').addClass('open') })
 
 
 
-
-
-
-
-});
+})
